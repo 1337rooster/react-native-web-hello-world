fcb8be851610d2292d6f1177d01c70c5478f33e0
Alex Amato <amato@Alexs-MacBook-Air.local>
2018-04-03 23:11:35 -0700

    trying to port it, not working

--- app/web/index.js
+++ app/web/index.js
@@ -3,10 +3,22 @@ import { render }     from 'react-dom';
 import Root           from './containers/Root';
 import configureStore from '../store/configureStore';
 
+import WebSearchBox from '../color/WebSearchBox';
+
+import Colors from '../color/colors'
+import ColorListItem from '../color/ColorListItem'
+
 // load our css
 require('./styles/style.less');
+
+const ColorSearchBox = WebSearchBox(ColorListItem);
 
 const store = configureStore();
 const rootElement = document.getElementById('root');
 
+render( <ColorSearchBox searchStore={new Colors()}/>, rootElement );
+
+/*
 render( <Root store={store} />, rootElement );
+*/
+
--- index.android.js
+++ index.android.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry,
+  StyleSheet,
+  Text,
+  View } from 'react-native';
 import Root           from './app/native/containers/Root';
 import configureStore from './app/store/configureStore.prod.js';
 
+import Colors from './app/color/colors'
+import ColorSearchBox from './app/mobile/ColorMobileSearchBox';
+
+/*
 const store = configureStore();
 
 class ReactNativeelloWorld extends Component {
@@ -12,5 +19,50 @@ class ReactNativeelloWorld extends Component {
     );
   }
 }
-
 AppRegistry.registerComponent('ReactNativeWebHelloWorld', () => ReactNativeelloWorld);
+*/
+
+export default class SearchBoxMobile extends Component {
+  render() {
+    return (
+      <ColorSearchBox searchStore={new Colors()}/>
+    );
+    /*
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );*/
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ReactNativeWebHelloWorld', () => SearchBoxMobile);
+
